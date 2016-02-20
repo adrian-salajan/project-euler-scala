cbd10be1cae0f1bebabe44871bb616f1da8827c0
adrian salajan <adrian salajan>
2016-02-20 21:38:42 +0200

    Problem 1
    sum multiples of 3 and 5

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.print(Problem1.getSumOfMultiplesBelow(1000));
+
+    }
+}
--- src/test/kotlin/tests.kt
+++ src/test/kotlin/tests.kt
@@ -1,17 +1,13 @@
 package demo 
 
-import kotlin.test.assertEquals
+import kotlin.test.assertEquals as same
 import org.junit.Test as test
 
 class TestSource() {
-    @test fun f() {
-        val example : KotlinGreetingJoiner = KotlinGreetingJoiner(Greeter("Hi"))
-        example.addName("Harry")
-        example.addName("Ron")
-        example.addName(null)
-        example.addName("Hermione")
 
-        assertEquals(example.getJoinedGreeting(), "Hi Harry and Ron and Hermione")
+    @test fun problem1() {
+        same(233168,  Problem1.getSumOfMultiplesBelow2(1000))
+        same(233168, KProblem1.getSumOfMultiplesBelow2(1000))
     }
 }
 
